04e968cbbb190e27edb733c48fcb9532791afb87
Heeseung Yun <30399072+HS-YN@users.noreply.github.com>
2018-07-31 14:32:14 +0800

    Update install_vundle.sh

--- install_vundle.sh
+++ install_vundle.sh
@@ -5,8 +5,10 @@ git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 rm -rf ~/.vimrc
 cp ./.vimrc ~/
 vim -E -c PluginInstall -c q
+# If get frozen while installing youcompleteme, execute the command again.
 
 # Install youcompleteme
 cd ~/.vim/bundle/youcompleteme && \
+  git submodule update --init --recursive && \
   ./install.py --clang-completer --java-completer
 
